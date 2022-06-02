0bacab95e065c996c5495b356e911ddaf765f55a
yphung282@gmail.com <yphung282@gmail.com>
2022-06-02 17:41:53 +0700

    add add_data script

---
+++ add_data.sql
@@ -0,0 +1,37 @@
+use quanlysanpham;
+
+-- taikhoan
+insert into taikhoan(TenDangNhap,MatKhau,HoTen,SoDienThoai,NgaySinh,Quyen) 
+values ("PhungNhuY","1","Phùng Như Ý","0383844686","2001-02-28","nhanvien");
+insert into taikhoan(TenDangNhap,MatKhau,HoTen,SoDienThoai,NgaySinh,Quyen) 
+values ("TruongThiThuPhuong","1","Trương Thị Thu Phương","0966504386","2001-07-04","nhanvien");
+insert into taikhoan(TenDangNhap,MatKhau,Quyen) 
+values ("NguyenThiTraMy","1","nhanvien");
+insert into taikhoan(TenDangNhap,MatKhau,HoTen,SoDienThoai,NgaySinh,Quyen) 
+values ("admin","1","","",null,"quanly");
+
+-- danh muc
+insert into danhmuc(TenDanhMuc, MoTa)
+values ("áo thun", "day la mo ta");
+insert into danhmuc(TenDanhMuc, MoTa)
+values ("áo sơ mi", "day la mo ta");
+insert into danhmuc(TenDanhMuc, MoTa)
+values ("quần cộc", "day la mo ta");
+insert into danhmuc(TenDanhMuc, MoTa)
+values ("quần bò", "day la mo ta");
+
+-- san pham
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("áo thun nam",1, "chiếc", 10, 50000, "S", "mota");
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("áo thun nam",1, "chiếc", 12, 50000, "M", "mota");
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("áo thun nữ",1, "chiếc", 11, 50000, "S", "mota");
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("áo thun nữ",1, "chiếc", 12, 50000, "M", "mota");
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("áo sơ mi nam",2, "chiếc", 2, 100000, "S", null);
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("Quần cộc đen",3, "chiếc", 5, 120000, "M", "");
+insert into sanpham(TenSanPham, MaDanhMuc, DonViTinh, SoLuong, DonGia, KichCo, MoTa)
+values ("quần bò GAP",4, "chiếc", 0, 100000, "L", "");
--- init_database.sql
+++ init_database.sql
@@ -6,9 +6,9 @@ create table TaiKhoan(
 	MaTaiKhoan int primary Key auto_increment,
     TenDangNhap varchar(32) not null,
     MatKhau varchar(32) not null,
-    HoTen nvarchar(64),
+    HoTen varchar(64),
     SoDienThoai varchar(11),
-    NgaySinh date,
+    NgaySinh date null,
     Quyen varchar(10) not null,
     NgayTao datetime default current_timestamp,
     NgayCapNhat datetime default current_timestamp on update current_timestamp,
@@ -18,7 +18,7 @@ create table TaiKhoan(
 create table HoaDon(
 	MaHoaDon int primary key auto_increment,
     MaTaiKhoan int not null, 
-    TenKhachHang nvarchar(64),
+    TenKhachHang varchar(64),
     SoDienThoai varchar(11),
     ThoiGianXuat datetime default current_timestamp,
     TongTien int not null,
@@ -27,7 +27,7 @@ create table HoaDon(
 
 create table DanhMuc(
 	MaDanhMuc int primary key auto_increment,
-    TenDanhMuc nvarchar(128) not null,
+    TenDanhMuc varchar(128) not null,
     MoTa text,
     NgayTao datetime default current_timestamp,
     NgayCapNhat datetime default current_timestamp on update current_timestamp,
@@ -36,12 +36,12 @@ create table DanhMuc(
 
 create table SanPham(
 	MaSanPham int primary key auto_increment,
-    TenSanPham nvarchar(128) not null,
+    TenSanPham varchar(128) not null,
     MaDanhMuc int,
-    DonViTinh nvarchar(32),
+    DonViTinh varchar(32),
     SoLuong int default 0,
     DonGia int,
-    KichCo nvarchar(32),
+    KichCo varchar(32),
     MoTa text,
     NgayTao datetime default current_timestamp,
     NgayCapNhat datetime default current_timestamp on update current_timestamp,
